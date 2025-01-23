dfdda3c6b6075395581f81f69895cb69267942c9
Biocan888num2 <bepecinplus@gmail.com>
2025-01-23 13:18:33 -0500

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -3,7 +3,7 @@ class FlightData:
 
     def __init__(self, price, origin_airport, destination_airport, out_date, return_date, stops):
         """
-        Constructor for initializing a new flight data instance with specific travel details.
+        Constructor for initializing a new flight data instance with specific travel details
 
         Parameters:
         - price: The cost of the flight.
@@ -24,20 +24,21 @@ class FlightData:
 def find_cheapest_flight(data):
     """
     Parses flight data received from the Amadeus API to identify the cheapest flight option among
-    multiple entries.
+    multiple entries
 
     Args:
         data (dict): The JSON data containing flight information returned by the API.
 
     Returns:
         FlightData: An instance of the FlightData class representing the cheapest flight found,
-        or a FlightData instance where all fields are 'NA' if no valid flight data is available.
+        or a FlightData instance where all fields are 'NA' if no valid flight data is available
 
-    This function initially checks if the data contains valid flight entries. If no valid data is found,
-    it returns a FlightData object containing "N/A" for all fields. Otherwise, it starts by assuming the first
-    flight in the list is the cheapest. It then iterates through all available flights in the data, updating
-     the cheapest flight details whenever a lower-priced flight is encountered. The result is a populated
-     FlightData object with the details of the most affordable flight.
+            >>> This function initially checks if the data contains valid flight entries 
+            >>> If no valid data is found it returns a FlightData object containing "N/A" for all fields
+            >>> Otherwise, it starts by assuming the first flight in the list is the cheapest 
+            >>> It then iterates through all available flights in the data,
+            >>> updating the cheapest flight details whenever a lower-priced flight is encountered
+            >>> The result is a populated FlightData object with the details of the most affordable flight
     """
 
     # Handle empty data if no flight or Amadeus rate limit exceeded
@@ -56,12 +57,15 @@ def find_cheapest_flight(data):
     first_flight = data['data'][0]
     canadian_price = 1.8 * float(first_flight["price"]["grandTotal"])
     lowest_price = round(canadian_price, 2)
+    
     # A flight with 2 segments will have 1 stop
     nr_stops = len(first_flight["itineraries"][0]["segments"]) - 1
     origin = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+    
     # Final destination is found in the last segment of the flight
     destination = first_flight["itineraries"][0]["segments"][nr_stops]["arrival"]["iataCode"]
     out_date = first_flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
+    
     # Return date is the first segment of the second itinerary
     return_date = first_flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
 
